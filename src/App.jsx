823459e7debc44fb52b932a228badc2acbbbdca5
Opeyemi Olatunbosun <olatunbosunopeyemi186035@gmail.com>
2023-05-06 01:47:27 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React from "react"
+import Navbar from "./components/Navbar.jsx"
+import Hero from "./components/Hero.jsx"
+import Card from "./components/Card.jsx"
+
+import data from "./data.js"
+
+
+function App() {
+    const cards = data.map(item => {
+        return (
+            <Card
+                key={item.id}
+                img={item.coverImg}
+                rating={item.stats.rating}
+                reviewCount={item.stats.reviewCount}
+                location={item.location}
+                title={item.title}
+                price={item.price}
+                
+            />
+        )
+    })        
+    
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+            <section className="cards-list">
+                {cards}
+            </section>
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Logo from "../assets/airbnb-logo.png"
+
+
+function Navbar() {
+    return (
+        <nav>
+            <img src={Logo} className="nav--logo" />
+        </nav>
+    )
+}
+
+export default Navbar
